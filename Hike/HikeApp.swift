661a8281b42e33176169051632e2a5352602077c
huseyindemirturk <hdemirturk@cyh.com.tr>
2024-12-30 16:35:43 +0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Hüseyin Demirtürk on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
